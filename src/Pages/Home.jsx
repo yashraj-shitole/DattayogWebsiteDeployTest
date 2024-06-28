463f29138cc3b2edf893ec87c6e11d9a02df5ce5
Yashraj <yashrajs927@gmail.com>
2024-06-28 20:47:02 +0530

    seo

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -387,6 +387,7 @@ const Home = () => {
             <div className="w-[40%] hidden lg:flex rounded-3xl overflow-hidden">
               <img
                 className="h-full object-cover"
+                loading="lazy"
                 src="https://images.unsplash.com/photo-1605000797499-95a51c5269ae?q=80&w=2342&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt=""
               />
